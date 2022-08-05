dec3e06f4e6471c5766f58c33d9c16ab7d214ad0
Your Name <timlauffs@gmail.com>
2022-08-05 20:13:44 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import jsonData from "./data.json";
+const tab = 1;
+const planetColor = jsonData[tab].color;
+const info = jsonData;
+</script>
+
+<template>
+  <div>
+    <img :src="'src/' + jsonData[tab].images.planet" alt="">
+    <h1>{{ jsonData[tab].name }}</h1>
+    <p>{{ jsonData[tab].overview.content }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.planet-color {
+  color: v-bind("planetColor");
+}
+</style>
